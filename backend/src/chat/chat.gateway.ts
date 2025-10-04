9ef4eaab601c35d46635984742f34f3efff477cb
danilap <danielnigatu09@gmail.com>
2025-10-04 17:51:44 +0300

    fix: chatgetway

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -7,8 +7,9 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@WebSocketGateway(3002, {
+@WebSocketGateway(4000, {
   cors: {
     origin: '*',
   },
@@ -16,6 +17,7 @@ import { Server, Socket } from 'socket.io';
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+  constructor(private prisma: PrismaService) {}
 
   handleConnection(client: Socket, ...args: any[]) {
     console.log('new connect id is:', client.id);
@@ -33,9 +35,34 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @SubscribeMessage('send_message')
   async handleMessage(
-    @MessageBody() data: { sender: string; content: string },
+    @MessageBody() body: { username: string; text: string }, // corrected type
   ) {
-    console.log('test', data);
-    this.server.emit('receive_message', data);
+    const data: { username: string; text: string } = JSON.parse(body as any);
+
+    if (!data.username) return;
+    console.log('Looking for user:', data.username);
+    let user = await this.prisma.user.findUnique({
+      where: { username: data.username },
+    });
+
+    if (!user) {
+      user = await this.prisma.user.create({
+        data: { username: data.username },
+      });
+    }
+
+    // Save message
+    const message = await this.prisma.message.create({
+      data: { text: data.text, userId: user.id },
+      include: { user: true },
+    });
+
+    // Broadcast to everyone (group chat)
+    this.server.emit('receive_message', {
+      id: message.id,
+      text: message.text,
+      username: message.user.username,
+      createdAt: message.createdAt,
+    });
   }
 }
